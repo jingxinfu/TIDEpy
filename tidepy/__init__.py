3fbf60ed0d3aa28a61dbdf4c966a14a7f51354d7
jingxinfu <jingxinfu.tj@gmail.com>
2020-02-06 18:06:12 -0500

    CI Fix

--- tidepy/__init__.py
+++ tidepy/__init__.py
@@ -1,6 +1,6 @@
 import os
 import pkg_resources
-__version__="1.2"
+__version__="1.3"
 DATA_DIR = pkg_resources.resource_filename('tidepy', 'data/')
 MODEL_DB_PATH = os.path.join(DATA_DIR,'model.pkl')
 GENE_REF_PATH = os.path.join(DATA_DIR,'Gene_Ref.pkl')
