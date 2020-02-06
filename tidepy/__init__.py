3da7f883b1b3a9247017b345fef7ee0f48d68178
jingxinfu <jingxinfu.tj@gmail.com>
2020-02-06 16:52:19 -0500

    test

--- tidepy/__init__.py
+++ tidepy/__init__.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import pkg_resources
-__version__="1.0"
+__version__="1.2"
 DATA_DIR = pkg_resources.resource_filename('tidepy', 'data/')
 MODEL_DB = pd.read_pickle(os.path.join(DATA_DIR,'model.pkl'))
 GENE_REF = pd.read_pickle(os.path.join(DATA_DIR,'Gene_Ref.pkl'))
